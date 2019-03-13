3d082aaa1ac01ec694304e896ae4ef3178efef2c
Josiel <josielalv@hotmail.com>
2019-03-13 12:24:22 -0300

    Aula 1 e 2

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola novo teste");
+
+            int idade = 37;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("a idade é " + idade + ", parabéns!");
+
+            Console.ReadLine();
+        }
+    }
+}
