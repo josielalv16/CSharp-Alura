0c5d402e06663e594d824d97603e295bf2d15e3e
Josiel <josielalv@hotmail.com>
2019-03-13 12:38:17 -0300

    Aula 3 criando variaveis ponto flutuante

--- 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -10,6 +10,25 @@ namespace _3_CriandoVariaveisPontoFlutuante
     {
         static void Main(string[] args)
         {
+            double salario = 1250.70;
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("meu salario é " + salario);
+
+            int valor = 12.5;             // não compila.
+
+            int valor2 = 0.0;             // não compila.
+
+            double teste = 125.50;        // compila
+
+            int valor3 = teste;           // não compila.
+
+            int divisao = 5 / 2;
+            Console.WriteLine(divisao);
+
+            double divisao2 = 5.0 / 2;
+            Console.WriteLine(divisao2);
         }
     }
 }
