2d52ae208ca8bdccbd83baf79031e6936b12759b
Josiel <josielalv@hotmail.com>
2019-03-13 22:09:02 -0300

    7-Condicionais

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/7-Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando condicionais");
+
+            int idade = 20;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("voce nao tem 18, mas pode entrar, pois está acompanhado");
+
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente voce nao pode entrar");
+                }
+            }
+        }
+    }
+}
