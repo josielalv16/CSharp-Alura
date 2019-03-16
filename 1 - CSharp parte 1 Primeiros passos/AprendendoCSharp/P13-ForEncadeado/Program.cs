398664f7dd2a9d59e2bb957c469ec3801f859c69
Josiel <josielalv@hotmail.com>
2019-03-16 16:23:56 -0300

    for encadeado

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrevendo os * com Break;
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+
+            // Uma forma diferente de fazer o desenho de *
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
