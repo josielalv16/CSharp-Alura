306fb4c2961ad3cd2a98bf8fa1c49661956f98fe
Josiel <josielalv@hotmail.com>
2019-03-16 16:56:21 -0300

    Laco de repeticao for

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/LacoDeRepeticaoFor3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoFor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna <= linha; coluna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
