361b5d91c6b9de0045ded62061497775fa88f7a5
Josiel <josielalv@hotmail.com>
2019-03-16 16:57:12 -0300

    Multiplos de 3

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/MultiplosDe3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
