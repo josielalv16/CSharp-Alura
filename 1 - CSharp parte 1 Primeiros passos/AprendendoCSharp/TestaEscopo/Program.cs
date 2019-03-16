ac7136b0eaa15b8f539d121f370bff7373406971
Josiel <josielalv@hotmail.com>
2019-03-16 14:09:26 -0300

    Testa escopo e Imposto de Renda

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/TestaEscopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaEscopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando condicionais");
+
+            int idade = 20;
+            int quantidadePessoas = 3;
+            bool acompanhado;
+
+            if (quantidadePessoas >= 2)
+            {
+                acompanhado = true;
+            }
+            else
+            {
+                acompanhado = false;
+            }
+
+            if (idade >= 18 || acompanhado)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("infelizmente voce nao pode entrar");
+            }
+        }
+    }
+}
