75370593972b357a9baf7d911dfa8476f153cc05
Josiel <josielalv@hotmail.com>
2019-03-16 18:09:43 -0300

    Classe conta e conta gabriela

--- 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/01-ByteBank/Program.cs
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/01-ByteBank/Program.cs
@@ -10,7 +10,21 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo " + contaDaGabriela.saldo);
+
 
             Console.ReadLine();
         }
