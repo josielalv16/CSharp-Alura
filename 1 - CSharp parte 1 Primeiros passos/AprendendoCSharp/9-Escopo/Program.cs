52ef959afaf5bc4c0449d7036e38d719e3d86e9e
Josiel <josielalv@hotmail.com>
2019-03-16 14:02:19 -0300

    9 Escopo

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 20;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if(acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+
+            if (idade >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem vindo");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
