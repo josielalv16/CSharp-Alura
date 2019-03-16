e34be43c5e4abf2c49d1043e30650112cfbb2562
Josiel <josielalv@hotmail.com>
2019-03-16 16:26:09 -0300

    laco de repeticao for e while

---
+++ 1 - CSharp parte 1 Primeiros passos/AprendendoCSharp/LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;  //mais usado!
+            }
+            Console.ReadLine();
+        }
+    }
+}
