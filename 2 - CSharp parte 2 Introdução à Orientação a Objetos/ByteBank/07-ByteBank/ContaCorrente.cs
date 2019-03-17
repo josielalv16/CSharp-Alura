18ed393f15dff6fbed57152d4f4b16e5ab015cff
Josiel <josielalv@hotmail.com>
2019-03-17 11:36:48 -0300

    Construtores

---
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,63 @@
+﻿namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
