7574b7dd2cabca33580bd01b28c536b46532afad
Josiel <josielalv@hotmail.com>
2019-03-17 11:09:06 -0300

    ObterSaldo e DefinirSaldo

---
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,55 @@
+﻿namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                return;
+            }
+
+            this.saldo = saldo;
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
