df88d1cff7ab24137cbab43230f8655ce7a9f531
Josiel <josielalv@hotmail.com>
2019-03-16 22:37:48 -0300

    Composicao de classes

--- 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/05-ByteBank/Program.cs
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/05-ByteBank/Program.cs
@@ -12,6 +12,23 @@ namespace _05_ByteBank
         {
             Cliente gabriela = new Cliente();
 
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
         }
     }
 }
