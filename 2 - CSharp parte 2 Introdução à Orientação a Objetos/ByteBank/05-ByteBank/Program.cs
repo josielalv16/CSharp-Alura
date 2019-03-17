b102811f12846304600c668adc756b9cea54c9b2
Josiel <josielalv@hotmail.com>
2019-03-16 22:44:27 -0300

    Referências nulas

--- 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/05-ByteBank/Program.cs
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/05-ByteBank/Program.cs
@@ -10,23 +10,30 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+            //Cliente gabriela = new Cliente();
 
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Desenvolvedora C#";
-            gabriela.cpf = "434.562.878-20";
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "434.562.878-20";
 
             ContaCorrente conta = new ContaCorrente();
 
-            conta.titular = gabriela;
+            //conta.titular = gabriela;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "434.562.878-20";
+            conta.titular.profissao = "Desenvolvedora C#";
+
             conta.saldo = 500;
             conta.agencia = 563;
             conta.numero = 5634527;
 
-            conta.titular.nome = "Gabriela Costa";
 
-            Console.WriteLine(gabriela.nome);
+            //Console.WriteLine(gabriela.nome);
             Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
 
             Console.ReadLine();
         }
