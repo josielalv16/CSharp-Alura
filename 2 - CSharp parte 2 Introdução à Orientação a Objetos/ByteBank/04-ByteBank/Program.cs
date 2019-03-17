69671afd203c32e8b3e7bd4c1b1c7722251f9d40
Josiel <josielalv@hotmail.com>
2019-03-16 21:56:39 -0300

    Nossa primeira funcao

---
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+            Console.ReadLine();
+        }
+    }
+}
