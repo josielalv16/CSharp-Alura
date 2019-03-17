47bbd7aa90e1631bf499f44a0bc9af73648bb054
Josiel <josielalv@hotmail.com>
2019-03-16 21:59:53 -0300

    Metodo sem retorno

--- 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/04-ByteBank/ContaCorrente.cs
+++ 2 - CSharp parte 2 Introdução à Orientação a Objetos/ByteBank/04-ByteBank/ContaCorrente.cs
@@ -7,9 +7,19 @@
 
     public bool Sacar(double valor)
     {
-        if (this.saldo)
+        if (this.saldo < valor)
         {
-
+            return false;
         }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
     }
 }
