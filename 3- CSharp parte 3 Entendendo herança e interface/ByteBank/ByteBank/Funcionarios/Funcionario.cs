c6535de03261c8f08cb11fb3e1a0bc0e86bfa249
Josiel <josielalv@hotmail.com>
2019-03-18 21:00:29 -0300

    Atalhos no VisualStudio e classe Funcionario

---
+++ 3- CSharp parte 3 Entendendo herança e interface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - Funcionario
+        // 1 - Diretor
+        // 2 - designer
+        // N - ...
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ 3- CSharp parte 3 Entendendo herança e interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
