c8bfac73b57096069cd3126674c5a7fc581124ea
Josiel <josielalv@hotmail.com>
2019-03-24 14:03:10 -0300

    Blocos Try Catc

--- 4 - CSharp parte 4 Entendendo exceções/ByteBank/ByteBank/Program.cs
+++ 4 - CSharp parte 4 Entendendo exceções/ByteBank/ByteBank/Program.cs
@@ -10,11 +10,46 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874150);
-
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
-
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro");
+            }
+            
             Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch(DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possivel fazer um divisão por 0");
+            }
+            
+
+            
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
