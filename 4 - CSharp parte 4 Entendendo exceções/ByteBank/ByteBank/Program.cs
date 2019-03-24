2c877d7351303f0816efdb29d7d0552ef8593316
Josiel <josielalv@hotmail.com>
2019-03-24 17:50:02 -0300

    Bloco finally

--- 4 - CSharp parte 4 Entendendo exceções/ByteBank/ByteBank/Program.cs
+++ 4 - CSharp parte 4 Entendendo exceções/ByteBank/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,37 @@ namespace ByteBank
     class Program
     {
         static void Main(string[] args)
+        {
+            CarregarContas();
+
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            LeitorDeArquivo leitor = null;
+            try
+            {
+                leitor = new LeitorDeArquivo("contas.txt");
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Exceção do tipo IOException capturada e tratada!");
+            }
+            finally
+            {
+                if (leitor != null)
+                {
+                    leitor.Fechar();
+                }
+            }
+        }
+
+        private static void TestaInnerException()
         {
             try
             {
@@ -29,9 +61,8 @@ namespace ByteBank
                 Console.WriteLine(e.InnerException.Message);
                 Console.WriteLine(e.InnerException.StackTrace);
             }
-
-            Console.ReadLine();
         }
+
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
