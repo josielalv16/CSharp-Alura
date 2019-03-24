40c4ab6eb411ef9a2871eaca3209a72cd8679b5b
Josiel <josielalv@hotmail.com>
2019-03-24 13:44:26 -0300

    Conhecendo exceções

---
+++ 4 - CSharp parte 4 Entendendo exceções/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+    }
+}
