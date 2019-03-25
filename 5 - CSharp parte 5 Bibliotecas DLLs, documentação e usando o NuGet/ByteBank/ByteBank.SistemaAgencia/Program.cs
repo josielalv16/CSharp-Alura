50b7c799fad892c6e0eef5c1fab23624e3df39ef
Josiel <josielalv@hotmail.com>
2019-03-25 20:39:13 -0300

    Referencias de projetos

--- 5 - CSharp parte 5 Bibliotecas DLLs, documentação e usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 5 - CSharp parte 5 Bibliotecas DLLs, documentação e usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(847, 123123);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
         }
     }
 }
