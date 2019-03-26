5fdf83ef4a51aee2588fea56ac51403feceb5a9c
Josiel <josielalv@hotmail.com>
2019-03-25 21:15:50 -0300

    Referencias de DLL

---
+++ 5 - CSharp parte 5 Bibliotecas DLLs, documentação e usando o NuGet/SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,22 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 455789);
+            Console.WriteLine(conta.Saldo);
+
+            string nome = "Guilherme";
+
+            Console.ReadLine();
+        }
+    }
+}
