bf1415a10f1c66a103fb0b232b2876a5ef53cb4a
Josiel <josielalv@hotmail.com>
2019-03-25 22:20:31 -0300

    Lidando com datas

--- 5 - CSharp parte 5 Bibliotecas DLLs, documentação e usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 5 - CSharp parte 5 Bibliotecas DLLs, documentação e usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,15 +12,36 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 123123);
+            DateTime dataFimPagamento = new DateTime(2019, 5, 16);
+            DateTime dataCorrente = DateTime.Now;
 
-            new ContaCorrente(123, 654456);
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
 
-            Funcionario funcionario = null;
+            string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
 
-            Console.WriteLine(conta.Numero);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
 
+            Console.WriteLine(mensagem);
             Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                if (quantidadeMeses == 1)
+                {
+                    return "1 mes";
+                }
+                return quantidadeMeses + " meses";
+            }
+            else if (timeSpan.Days > 7)
+            {
+                int quantidadeSemanas = timeSpan.Days / 7;
+            }
+            return timeSpan.Days + " dias";
         }
     }
 }
