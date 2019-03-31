f272c33d844731487e7269f5f593441d1009ecde
Josiel <josielalv@hotmail.com>
2019-03-31 16:19:38 -0300

    IsNullOrEmpty e palavra chave string

---
+++ 6 - CSharp parte 6 String, expressões regulares e a classe Object/ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if(url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+        }
+    }
+}
---
+++ 6 - CSharp parte 6 String, expressões regulares e a classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,76 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(6);
+            Console.WriteLine(argumentos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //DateTime dataFimPagamento = new DateTime(2019, 5, 16);
+            //DateTime dataCorrente = DateTime.Now;
+
+            //TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            //string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            //Console.WriteLine(dataCorrente);
+            //Console.WriteLine(dataFimPagamento);
+
+            //Console.WriteLine(mensagem);
+            Console.ReadLine();
+        }
+
+        //static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        //{
+        //    if (timeSpan.Days > 30)
+        //    {
+        //        int quantidadeMeses = timeSpan.Days / 30;
+        //        if (quantidadeMeses == 1)
+        //        {
+        //            return "1 mes";
+        //        }
+        //        return quantidadeMeses + " meses";
+        //    }
+        //    else if (timeSpan.Days > 7)
+        //    {
+        //        int quantidadeSemanas = timeSpan.Days / 7;
+        //    }
+        //    return timeSpan.Days + " dias";
+        //}
+    }
+}
